6fdfe6ef2f08e0e19625aa54a43303c68827a963
Ghufron Fikrianto <ghufronfr@gmail.com>
2017-08-18 17:39:53 +0700

    Fix Form Validation, add animate.css

--- public/javascripts/sarahah.js
+++ public/javascripts/sarahah.js
@@ -1,10 +1,32 @@
-$(function () {
-  $('#msg-form').parsley().on('field:validated', function() {
-    var ok = $('.parsley-error').length === 0;
-    $('.bs-callout-info').toggleClass('hidden', !ok);
-    $('.bs-callout-warning').toggleClass('hidden', ok);
-  })
-  .on('form:submit', function() {
-    return false; // Don't submit form for this demo
+// $(function () {
+//   $('#msg-form').parsley().on('field:validated', function() {
+//     var ok = $('.parsley-error').length === 0;
+//     $('.bs-callout-info').toggleClass('hidden', !ok);
+//     $('.bs-callout-warning').toggleClass('hidden', ok);
+//   })
+//   .on('form:submit', function() {
+//     return false; // Don't submit form for this demo
+//   });
+// });
+
+$(document).ready(function(){
+
+  $('#button-submit').click(function (){
+    // var form_data = $('#msg-form').find('textarea[name="message-box"]').val();
+    $("#message-box").each(function(){
+        if ($.trim($(this).val()).length == 0){
+            $('.alert').show();
+            // $(this).addClass("highlight");
+            // isFormValid = false;
+
+        }
+        else{
+            // $(this).removeClass("highlight");
+            $('.alert').hide();
+            // isFormValid = true;
+         }
+        });
+
+    return false;
   });
 });
