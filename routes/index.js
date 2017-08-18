244c7f9b14b077d4de0d440d6262a09f5ba61f45
Ghufron Fikrianto <ghufronfr@gmail.com>
2017-08-18 15:19:39 +0700

    Adding Form Functionallity

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var expressValidator = require('express-validator');
+
+var app = express();
+
+
+app.use(expressValidator());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sarahaha - Ghufron Fikrianto ' });
+  io.on('connection', function(socket){
+    console.log('a user connected');
+  })
+});
+
+router.post('/msg', function(req, res, next) {
+  req.check('message-box', 'Invalid message').notEmpty();
+
+
+  req.getValidationResult().then(function(result) {
+    if (!result.isEmpty()) {
+      return;
+    }else{
+      console.log(req);
+      return;
+    }
+  });
+
+
 });
 
 module.exports = router;
